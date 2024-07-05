3d356967445b1db41816fca715fab7289adbbdc7
DvdTd <89341571+DvdTd@users.noreply.github.com>
2024-07-05 17:01:01 +0100

    Experiment BCs on a simpler example

--- Drift-Diffusion with energy.py
+++ Drift-Diffusion with energy.py
@@ -1,30 +1,26 @@
 
-# pypde cartesian is up to 3 dimensions, energy takes one, so effectively only two.
-# Let x = epsilon so y and/or z can be used for position
-# Can't use grads as they involve energy
+# py-pde cartesian is up to 3 dimensions so energy uses x, then y,z left for position.
 
-import pde
+import pde # import from installing py-pde
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
 
 # Variables
 dt = 1e-1
-maxTime = 10e1
+maxTime = 1e1
 energyRange = [-3, 3] # ±infinity but cutoff when it goes to zero
 positionRange = [-3, 3] # solar cell about 10cm
-numEnergyPoints = 50
-numPositionPoints = 50
+numEnergyPoints = 70
+numPositionPoints = 70
 
 dimension = 1 # accepts 1 or 2
 F = np.array([1e5]) # Tress p56, reasonably strong field is 1e5 or 1e6 V/cm
 numPlots = 10 # Number of plots, setting to 0 gives all
 maxGraphsPerRow = 5
 
-shouldUseBCs = True
-taskType = "gridSearch" # Options: timeEvo, gridSearch
+taskType = "timeEvo" # Options: timeEvo, gridSearch
 plotType = "mesh" # Used for timeEvo, options: mesh, colour2d, final
 
 
@@ -34,7 +30,7 @@ plotType = "mesh" # Used for timeEvo, options: mesh, colour2d, final
 # initialField = f"{np.e}**(-(x-{energyRange[1] - energyRange[0]}/2)**2/{energyRange[1] - energyRange[0]}**2)"
 # initialField = "heaviside(x, 0)*(x)"
 # initialField = "heaviside( -(x)*(x-2), 0)"
-# initialField = f"{np.e}**(-(x)**2)"
+# initialField = f"{np.e}**(-(x-1)**2)"
 
 # initialField = f"cos({np.pi}/({energyRange[1]} - {energyRange[0]})*x)*cos({np.pi}/({positionRange[1]} - {positionRange[0]})*y)"
 # initialField = f"cos(2*{np.pi}/({positionRange[1]} - {positionRange[0]})*y)"
@@ -45,12 +41,13 @@ initialField = f"{np.e}**(-(y)**2)"
 
 # Constants
 kb = 1.3806e-23
+eCharge = 1.602e-19
 gamma = 0.788 # Bilayers p8 0.788
 nu0 = 1
 g1 = 1
-sigma = 0.13 # Gaussian width of g2(e) - Tress p55 50meV?, p51 50-150meV, Traps p5 0.13eV
+sigma = 0.1 # Gaussian width of g2(e) - Tress p55 5e-2eV?, p51 0.05->0.15 eV, Traps p5 0.13eV
 Ec = 0 # Gaussian centre of g2(e) - Traps p5 -5.2eV although just offsets all energy
-Lambda = 0.1e-3 # 1e-3 # Tress p114, 1e-2 to 1e-4 (cm/V)^(1/2) CHECK AS CM
+Lambda = 9e-5 # 1e-3 # Tress p114, 1e-2 to 1e-4 (cm/V)^(1/2) CHECK AS CM, 9e-6 or 9e-5eV Alexandros
 T = 300 # temperature, 300 Tress p63, why so cold? or p92 273+25 = 398, Traps p5 also 300
 # p104? chemical potential with a distance of 0.5 eV (T = 300 K) away from the center of the Gaussian DOS
 # reorganisation energy, Tress p61 - "dissociation mechanism of this state with binding energies between 0.2 and 0.5 eV is still unclear"
@@ -60,7 +57,7 @@ T = 300 # temperature, 300 Tress p63, why so cold? or p92 273+25 = 398, Traps p5
 def calculatePDE(dt=dt, F = F, sigma=sigma, Lambda=Lambda):
     K = [1/4*gamma**(-3), 3*np.pi/8*gamma**(-4), np.pi*gamma**(-5)]
     C = [gamma**(-1), np.pi/2*gamma**(-2), np.pi*gamma**(-3)]
-    beta = 1/(kb*T)
+    beta = 1/(kb*T) * eCharge # multiply by charge to get eV units
     sigmaTilde = np.sqrt(sigma**2 + 2*Lambda/beta)
 
     factor = f"{nu0}*{g1}*(2*{np.pi})**(-1/2)*{sigmaTilde}**(-2) * {np.e}**(-1/2*{sigmaTilde}**(-2) * (x - {Ec} - {Lambda})**2)"
@@ -73,24 +70,16 @@ def calculatePDE(dt=dt, F = F, sigma=sigma, Lambda=Lambda):
         dotGradTerm = f"{F[0]} * d_dy(n) + {F[1]} * d_dz(n)" 
         laplaceTerm = "d_dy(d_dy(n)) + d_dz(d_dz(n))"
 
-    if shouldUseBCs == True:
-        bc_x =  "dirichlet" # what BCs show energy have? 0 density at 0 energy? dirichlet or neumann, ["dirichlet", "dirichlet"]
-        bc_y =  "periodic" # [{"value": "0"}, {"value": "0"}]
-        eq = pde.PDE({"n": f"{factor} * ( {K[dimension-1]}*{beta}/2*{dotGradTerm} + {K[dimension-1]}/2*{laplaceTerm} - {C[dimension-1]}*{EBar}*d_dx(n) + {C[dimension-1]}*({EBar}**2 + 2*{Lambda}*{sigma}**2/{beta}*{sigmaTilde}**(-2))*d_dx(d_dx(n)) )"}, bc=[bc_x, bc_y])               
-    else:
-        eq = pde.PDE({"n": f"{factor} * ( {K[dimension-1]}*{beta}/2*{dotGradTerm} + {K[dimension-1]}/2*{laplaceTerm} - {C[dimension-1]}*{EBar}*d_dx(n) + {C[dimension-1]}*({EBar}**2 + 2*{Lambda}*{sigma}**2/{beta}*{sigmaTilde}**(-2))*d_dx(d_dx(n)) )"})               
-
+    bc_x =  "dirichlet" # what BCs show energy have? 0 density at 0 energy? dirichlet or neumann, ["dirichlet", "dirichlet"]
+    bc_y =  "periodic" # [{"value": "0"}, {"value": "0"}]
+    eq = pde.PDE({"n": f"{factor} * ( {K[dimension-1]}*{beta}/2*{dotGradTerm} + {K[dimension-1]}/2*{laplaceTerm} - {C[dimension-1]}*{EBar}*d_dx(n) + {C[dimension-1]}*({EBar}**2 + 2*{Lambda}*{sigma}**2/{beta}*{sigmaTilde}**(-2))*d_dx(d_dx(n)) )"}, bc=[bc_x, bc_y])               
     grid = pde.CartesianGrid([energyRange, positionRange], [numEnergyPoints,numPositionPoints], periodic=[False, True])
 
     # Initial field
-    state = pde.ScalarField.from_expression(grid, initialField + f"* heaviside( (x-{energyRange[0]})*(x-{energyRange[1]})*(y-{positionRange[0]})*(y-{positionRange[1]}), 0) ")
+    state = pde.ScalarField.from_expression(grid, initialField + f"* heaviside( -(x-{energyRange[0]})*(x-{energyRange[1]}), 0) * heaviside( -(y-{positionRange[0]})*(y-{positionRange[1]}), 0) ")
 
     storage = pde.MemoryStorage() # store intermediate information of the simulation
     res = eq.solve(state, t_range=maxTime, dt=dt, tracker=["progress", storage.tracker(dt)])  # solve the PDE
-
-    
-    
-
     return res, storage
 
 
@@ -138,7 +127,7 @@ if taskType == "timeEvo":
                 # print(field.data)
                 ax.plot_surface(X, Y, field.data.T, cmap=cm.coolwarm)
                 ax.set_box_aspect(aspect=None, zoom=0.9)
-                ax.set(xlabel="Energy", ylabel="Position", zlabel="Electron density", title=f"Time = {round(time, decimalPlaces)}s")
+                ax.set(xlabel="Energy", ylabel="Position", zlabel="", title=f"n, time = {round(time, decimalPlaces)}s")
                 numGraph += 1
 
     elif plotType == "colour2d":
@@ -147,7 +136,7 @@ if taskType == "timeEvo":
 
         # Removes extra axes
         for removingIndex in range(columns-1 - (numPlots-1)%maxGraphsPerRow):
-            print(rows-1, columns - removingIndex - 1)
+            # print(rows-1, columns - removingIndex - 1)
             axs[rows-1, columns - removingIndex - 1].remove()
 
         numGraph = 0
@@ -182,6 +171,7 @@ if taskType == "timeEvo":
 
     plt.show()
 
+
 if taskType == "gridSearch":
     energies = np.linspace(energyRange[0], energyRange[1], numEnergyPoints)
     positions = np.linspace(positionRange[0], positionRange[1], numPositionPoints)
@@ -197,15 +187,17 @@ if taskType == "gridSearch":
     # Two parameters to vary in a grid
     parameterData = [{"name": "dt", "range": [-1.7, 0], "numPoints": 6, "defaultValue": 1e-1, "isLog": True},
                      {"name": "F", "range": [1e5, 1e6], "numPoints": 6, "defaultValue": 1e5, "isLog": False},
-                     {"name": "σ", "range": [0.02, 0.20], "numPoints": 6, "defaultValue": 0.13, "isLog": False},
-                     {"name": "λ", "range": [-3, -6], "numPoints": 6, "defaultValue": -4, "isLog": True}]
+                     {"name": "σ", "range": [0.02, 0.20], "numPoints": 3, "defaultValue": 0.13, "isLog": False},
+                     {"name": "λ", "range": [-0, -6], "numPoints": 2, "defaultValue": 9e-5, "isLog": True}] #[-0, -6]
     
     # Set parameters:
-    chosenDataIndices = [0]
+    chosenDataIndices = [2, 3]
 
+    numChosen = len(chosenDataIndices)
     numData = len(parameterData)
     points = [0] * numData
     numParamIterations = [1] * numData
+    output = []
 
     for i in range(numData):
         if i in chosenDataIndices:
@@ -215,10 +207,11 @@ if taskType == "gridSearch":
             else:
                 points[i] = np.linspace(parameterData[i]["range"][0], parameterData[i]["range"][1], parameterData[i]["numPoints"])
 
-    output = []
-    # if numData == 2:
-    #     fig = plt.figure(figsize=(3*parameterData[chosenDataIndices[1]]["numPoints"]+1,4*parameterData[chosenDataIndices[0]]["numPoints"]))
-    
+    shouldPlotGrid = True
+    if shouldPlotGrid == True and numChosen == 2:
+        rows = parameterData[chosenDataIndices[0]]["numPoints"]
+        columns = parameterData[chosenDataIndices[1]]["numPoints"]
+        fig = plt.figure(figsize=(3*rows+1,4*columns))
 
     for i0 in range(numParamIterations[0]):
         if 0 in chosenDataIndices:
@@ -259,23 +252,25 @@ if taskType == "gridSearch":
                     # dt, F, sigma, lambda, timeBeforeNaN
                     output.append([val0, val1, val2, val3, timeBeforeNaN])
 
-                # X, Y = np.meshgrid(energies, positions)
-                # fig = plt.figure(figsize=(8,8))
-                # ax = fig.add_subplot(1, 1, 1, projection='3d')
-                # ax.plot_surface(X, Y, res.data.T, cmap=cm.coolwarm)
-                # ax.set_box_aspect(aspect=None, zoom=0.9)
-                # ax.set(xlabel="Energy", ylabel="Position", zlabel="Electron density", title=f"")
-                # plt.show()
+                # WORK IN PROGRESS
+                    if shouldPlotGrid == True and numChosen == 2:
+                        indices = [i0, i1, i2, i3]
+                        X, Y = np.meshgrid(energies, positions)
+                        print(rows, columns, columns*indices[chosenDataIndices[0]]+indices[chosenDataIndices[1]]+1)
+                        ax = fig.add_subplot(rows, columns, columns*indices[chosenDataIndices[0]]+indices[chosenDataIndices[1]]+1, projection='3d')
+                        ax.plot_surface(X, Y, res.data.T, cmap=cm.coolwarm)
+                        ax.set_box_aspect(aspect=None, zoom=0.9)
+                        # {parameterData[chosenDataIndices[0]]["name"]}={points[chosenDataIndices[0]][indices[chosenDataIndices[0]]]}, {parameterData[chosenDataIndices[1]]["name"]}={points[chosenDataIndices[1]][indices[chosenDataIndices[1]]]}
+                        ax.set(xlabel="Energy", ylabel="Position", zlabel="Electron density", title=f"")
 
     
     output.sort(key=lambda x: x[-1],reverse=True)
     for arr in output:
         print(arr, ",")
     # print(output)
-    # print("-----------")
 
 
-
-    # plt.show()
+    if shouldPlotGrid == True and numChosen == 2:
+        plt.show()
 
 
--- Py-PDE example.py
+++ Py-PDE example.py
@@ -18,26 +18,54 @@
 
 # plot_kymograph(storage)  # visualize the result in a space-time plot
 
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+import pde
 
-from pde import DiffusionPDE, ScalarField, UnitGrid
-
-grid = UnitGrid([32, 32], periodic=[True, True])  # generate grid
-state = ScalarField.random_uniform(grid, 0.2, 0.3)  # generate initial condition
-
-field = ScalarField.from_expression(grid, "x+2*y")
+energyRange = [-3, 3] # ±infinity but cutoff when it goes to zero
+positionRange = [-3, 3] # solar cell about 10cm
+numEnergyPoints = 100
+numPositionPoints = 100
+energies = np.linspace(energyRange[0], energyRange[1], numEnergyPoints)
+positions = np.linspace(positionRange[0], positionRange[1], numPositionPoints)
+small = 1e0
+# grid = pde.UnitGrid([32, 32], periodic=[False, True])  # generate grid
+grid = pde.CartesianGrid([energyRange, positionRange], [numEnergyPoints,numPositionPoints], periodic=[False, False])
+# state = pde.ScalarField.random_uniform(grid, 0.2, 0.3)  # generate initial condition
+heaviside = f"* heaviside( -(x-{energyRange[0]}-{small})*(x-{energyRange[1]}+{small}), 0) * heaviside( -(y-{positionRange[0]}-{small})*(y-{positionRange[1]}+{small}), 0) "
+# heaviside = f"* heaviside( -(x-2)*(x+2), 0)  * heaviside( -(y-2)*(y+2), 0)"
+field = pde.ScalarField.from_expression(grid, "2.718**(-(x/2)**2-(y/2)**2)"+heaviside)
 
 
 # set boundary conditions `bc` for all axes
 
-bc_x = "periodic"
-bc_y = "periodic"
-eq = DiffusionPDE(bc=[bc_x, bc_y])
+# bc_x = "dirichlet"
+# bc_y = "dirichlet"
+# bc_x_left = {"derivative": 0}
+# bc_x_right = {"value": "sin(y / 2)"}
+bc_x_left = {"value": "0"}
+bc_x_right = {"value": "0"}
+bc_x = [bc_x_left, bc_x_right]
+bc_x = "dirichlet"
+bc_y = "neumann"
+# eq = DiffusionPDE(bc=[bc_x, bc_y])
+eq = pde.PDE({"n": f"d_dx(d_dx(n))"}, bc=[bc_x, bc_y])               
 
-result = eq.solve(field, t_range=10, dt=0.005)
-result.plot()
+
+res = eq.solve(field, t_range=0.01, dt=0.005)
+# result.plot()
+
+X, Y = np.meshgrid(energies, positions)
+fig = plt.figure(figsize=(8,8))
+
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+ax.plot_surface(X, Y, res.data.T, cmap=cm.coolwarm)
+ax.set_box_aspect(aspect=None, zoom=0.9)
+ax.set(xlabel="x", ylabel="y", zlabel="Electron density", title=f"")
 
 
-
+plt.show()
 
     # Plot the results:
     # Decimal precision from dt.
