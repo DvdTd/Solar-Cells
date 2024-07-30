7f70738ac6fafeab92fed71a02a8339af98d6e60
DvdTd <89341571+DvdTd@users.noreply.github.com>
2024-07-30 17:29:47 +0100

    Implemented drift diffusion equations using MethodOfLines and plotted as a mesh gif.

--- LearningJulia.jl
+++ LearningJulia.jl
@@ -1,31 +1,44 @@
-using MethodOfLines, ModelingToolkit, OrdinaryDiffEq, DomainSets, Plots
+# using MethodOfLines, ModelingToolkit, OrdinaryDiffEq, DomainSets, Plots
 
-@parameters t, x
-@variables u(..) cumuSum(..)
-Dt = Differential(t)
-Dx = Differential(x)
-xmin = 0.0
-xmax = 2.0pi
+# @parameters t, x
+# @variables u(..) cumuSum(..)
+# Dt = Differential(t)
+# Dx = Differential(x)
+# xmin = 0.0
+# xmax = 2.0pi
 
-# Integral limits are defined with DomainSets.ClosedInterval
-Ix = Integral(x in DomainSets.ClosedInterval(xmin, x)) # basically cumulative sum from 0 to x
+# # Integral limits are defined with DomainSets.ClosedInterval
+# Ix = Integral(x in DomainSets.ClosedInterval(xmin, x)) # basically cumulative sum from 0 to x
 
-eq = [
-    cumuSum(t, x) ~ Ix(u(t, x)), # Note wrapping the argument to the derivative with an auxiliary variable
-    Dt(u(t, x)) + 2 * u(t, x) + 5 * cumuSum(t, x) ~ 1
-]
-bcs = [u(0.0, x) ~ cos(x), Dx(u(t, xmin)) ~ 0.0, Dx(u(t, xmax)) ~ 0]
+# eq = [
+#     cumuSum(t, x) ~ Ix(u(t, x)), # Note wrapping the argument to the derivative with an auxiliary variable
+#     Dt(u(t, x)) + 2 * u(t, x) + 5 * cumuSum(t, x) ~ 1
+# ]
+# bcs = [u(0.0, x) ~ cos(x), Dx(u(t, xmin)) ~ 0.0, Dx(u(t, xmax)) ~ 0]
 
-domains = [t ∈ Interval(0.0, 2.0), x ∈ Interval(xmin, xmax)]
+# domains = [t ∈ Interval(0.0, 2.0), x ∈ Interval(xmin, xmax)]
 
-@named pde_system = PDESystem(eq, bcs, domains, [t, x], [u(t, x), cumuSum(t, x)])
+# @named pde_system = PDESystem(eq, bcs, domains, [t, x], [u(t, x), cumuSum(t, x)])
 
-order = 2
-discretization = MOLFiniteDifference([x => 30], t)
+# order = 2
+# discretization = MOLFiniteDifference([x => 30], t)
 
-prob = MethodOfLines.discretize(pde_system, discretization)
-sol = solve(prob, QNDF(), saveat = 0.1);
+# prob = MethodOfLines.discretize(pde_system, discretization)
+# sol = solve(prob, QNDF(), saveat = 0.1);
 
-solu = sol[u(t, x)]
+# solu = sol[u(t, x)]
 
-display(plot(sol[x], transpose(solu)))
+# display(plot(sol[x], transpose(solu)))
+
+
+# # println(min([1,2,3]...))
+using Printf, Formatting
+x = 1f-3
+
+
+function test(n,x)
+    
+    Printf.format(Printf.Format("%.2e"),x)
+end
+
+Printf.format(Printf.Format("%.2e"),x)
--- Using_MethodOfLines.jl
+++ Using_MethodOfLines.jl
@@ -1,4 +1,4 @@
-using MethodOfLines, ModelingToolkit, OrdinaryDiffEq, DomainSets, Plots
+using MethodOfLines, ModelingToolkit, OrdinaryDiffEq, DomainSets, Plots, Printf
 
 d = 2 # dimension
 kb = 1.3806f-23
@@ -14,7 +14,7 @@ Lambda = 9e-5#9e-5 # 9e-6 or 9e-5eV Alexandros
 T = 5772 # Tress p63 300K
 K = [1/4*gamma^(-3), 3*π/8*gamma^(-4), π*gamma^(-5)][clamp(d-1, 1, 3)]
 C = [gamma^(-1), π/2*gamma^(-2), π*gamma^(-3)][clamp(d-1, 1, 3)]
-F = [1e3][1] # Tress p56, reasonably strong field is 1e5 or 1e6 V/cm
+F = [1f5][1] # Tress p56, reasonably strong field is 1e5 or 1e6 V/cm
 beta = 1/(kb*T) * eCharge # multiply by charge to get eV units
 sigmaTilde = sqrt(sigma^2 + 2*Lambda/beta)
 # Light interaction Constants
@@ -29,16 +29,16 @@ N = 5f20
 solidAngleCoeff = 1f-5
 
 ## Definition of the problem
-numPlots = 4
-dt = 1e-11# 5e-4#1e-11
-maxTime = 1.0#5e-6# 1e1#5e-6
+numPlots = 50
+dt = 1f-11# 5e-4#1e-11
+maxTime = 1e-3# 1e1#5e-6
 energyRange = [-1.0, 1.0] # ±infinity but cutoff when it goes to zeros
 positionRange = [-10.0, 10.0] 
-numEnergyPoints = 100
-numPositionPoints = 100 
+numEnergyPoints = 25
+numPositionPoints = 25
 
 @parameters t, ϵ, x
-@variables u(..) cumuSum(..)
+@variables u(..) Ebar(..)
 Dt = Differential(t)
 Dϵ = Differential(ϵ)
 Dx = Differential(x)
@@ -47,24 +47,36 @@ normalGaussian(x, mean, width) = (2*π)^(-0.5)*width^(-2) * exp(- sum((x .- mean
 Ix = Integral(x in DomainSets.ClosedInterval(positionRange[1], x)) # basically cumulative sum from 0 to x
 
 eq = [
-    cumuSum(t, x) ~ Ix(u(t, x)), # Note wrapping the argument to the derivative with an auxiliary variable
-    Dt(u(t, x)) + 2 * u(t, x) * u(t, x) + 5 * cumuSum(t, x) ~ 1
+    Ebar(t, ϵ, x) ~ Lambda*sigmaTilde^(-2) * (2/beta*(ϵ-Ec) + sigma^2),
+    Dt(u(t, ϵ, x)) ~ exp(-beta*E)*nu0*g1*(2*pi)^(-0.5)*sigmaTilde^(-0.5) * exp(-(ϵ-Ec-Lambda)^2*0.5*sigmaTilde^(-2)) * (  K*beta/2*F * Dx(u(t, ϵ, x)) + K/2 * Dx(Dx(u(t, ϵ, x))) - C*Ebar(t, ϵ, x) * Dϵ(u(t, ϵ, x)) + C*(Ebar(t, ϵ, x)^2 + 2*Lambda*sigma^2/beta*sigmaTilde^(-2)) * Dϵ(Dϵ(u(t, ϵ, x))) )
 ]
 
-# Neumann examples: Dx(u(t, xmax)) ~ 0
-initialFunc(x) = normalGaussian(x, 0, 1) - normalGaussian(positionRange[1], 0, 1)
-bcs = [u(0.0, x) ~ initialFunc(x), u(t, positionRange[1]) ~ 0.0, u(t, positionRange[2]) ~ 0] 
+initialFunc(ϵ, x) = normalGaussian(x, 0, 1) - normalGaussian(positionRange[1], 0, 1)
+bcs = [u(0.0, ϵ, x) ~ initialFunc(ϵ, x),
+    #   Dx(u(t, energyRange[1], x)) ~ 0.0,
+       Dϵ(u(t, energyRange[2], x)) ~ 0.0,
+       u(t, ϵ, positionRange[1]) ~ 0.0, 
+       u(t, ϵ, positionRange[2]) ~ 0.0] 
 
-domains = [t ∈ Interval(0.0, maxTime), x ∈ Interval(positionRange[1], positionRange[2])]
+domains = [t ∈ Interval(0.0, maxTime), ϵ ∈ Interval(energyRange[1], energyRange[2]), x ∈ Interval(positionRange[1], positionRange[2])]
 
-@named pde_system = PDESystem(eq, bcs, domains, [t, x], [u(t, x), cumuSum(t, x)])
+@named pde_system = PDESystem(eq, bcs, domains, [t, ϵ, x], [u(t, ϵ, x), Ebar(t, ϵ, x)])
 
 order = 2
-discretization = MOLFiniteDifference([x => numPositionPoints], t)
+discretization = MOLFiniteDifference([ϵ => numEnergyPoints, x => numPositionPoints], t)
 
 prob = MethodOfLines.discretize(pde_system, discretization)
-sol = solve(prob, QNDF(), saveat = maxTime/numPlots)
+sol = solve(prob, QNDF(), saveat = maxTime/numPlots, dt=dt)
+solu = sol[u(t, ϵ, x)]
+zmin = min(solu...)
+zmax = max(solu...)
 
-solu = sol[u(t, x)]
+anim = @animate for i in 1:length(sol[t])
+    plot = surface(sol[ϵ], sol[x], transpose(solu[i, :, :]), xlabel="Energy", ylabel="Position", zlabel="n", camera=(80, 50), color=reverse(cgrad(:RdYlBu_11)), clims=(zmin, zmax))
+    zlims!(zmin, zmax)
+    titleStr = Printf.format(Printf.Format("%.2e"),(i-1)/length(sol[t]) * maxTime)
+    
+    title!("Time = " * titleStr * "s")
+end
+gif(anim, "drift_diffusion.gif", fps = 10)
 
-display(plot(sol[x], transpose(solu)))
